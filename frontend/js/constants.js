b4a11bdd09bfbd2e28b5e1cb63d119d6504cef49
Gandalf <jemshov@yahoo.com>
2022-04-08 21:40:57 +0400

    updated new colors

--- frontend/js/constants.js
+++ frontend/js/constants.js
@@ -1,11 +1,11 @@
 const contractAddress = '0x58dd0f81cdD513372d38fAD980eD43d2D0EE49A3' //Replace with your own contract address
 const chain = 'rinkeby' // rinkeby or polygon
 
-const welcome_h1 = 'Welcome to the CodeCats NFT Project!!'
+const welcome_h1 = 'Welcome to the Kuku Lala NFT Project!!'
 const welcome_h2 = 'Connect to MetaMask to Get Started'
 const welcome_p =
-  'The CodeCats NFT Project is a decentralized, open-source project that aims to demonstrate how to develope and launch your own NFT Collection. Follow step by step on the <a href="https://youtube.com/codestackr"  target="_blank" rel="noopener noreferrer">codeSTACKr</a> YouTube channel.'
-const h1_presale_coming_soon = 'NFT Drop Coming Soon!!'
+  'The Kuku Lala NFT Project is a decentralized, open-source project that aims to bring NFT holders to a new level, let them be able to use those NFTs to get engaged in Play-to-Earn games and generate income. For question please reach us out in the  <a href="https://t.me/kukulalanft"  target="_blank" rel="noopener noreferrer">Kuku Lala NFTs</a> Telegram group.'
+const h1_presale_coming_soon = 'NFT Pre-sale Coming Soon!!'
 const h1_presale_mint = 'Pre-Sale Minting Open!!'
 const h1_public_mint = 'Public Minting Open!!'
 const h2_presale_coming_soon = 'Pre-Sale Minting Countdown'
