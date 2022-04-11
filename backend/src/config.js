e1185f57ccca1d53b8f53a0dcca0adc3959a202f
Gandalf <jemshov@yahoo.com>
2022-04-11 13:56:18 +0400

    opensea change

--- backend/src/config.js
+++ backend/src/config.js
@@ -63,7 +63,7 @@ const TOKENS_PER_MINT = 3 // maximum number of NFTs a user can mint in a single 
 const PUBLIC_MINT_START_DATE = '2022-04-21T11:30:48+00:00' // This is required. Eg: 2022-02-08T11:30:48+00:00
 
 // OPTIONAL CONTRACT DETAILS THAT CAN BE UPDATED LATER.
-const PRESALE_MINT_START_DATE = '2022-04-10T03:00:00+00:00' // Optional. Eg: 2022-02-08T11:30:48+00:00
+const PRESALE_MINT_START_DATE = '2022-04-19T03:00:00+00:00' // Optional. Eg: 2022-02-08T11:30:48+00:00
 const ROYALTY_SHARE = 1000 // Percentage of the token price that goes to the royalty address. 100 bps = 1%
 const ROYALTY_ADDRESS = '0xEb45AB6B7BE6cBA3981Ed9199C2f52eC27455bcC' // Address that will receive the royalty
 const BASE_URI = null // only update if you want to manually set the base uri
